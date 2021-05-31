7b54a95ad40f2a5a7b53a74fcb341db32a2e496c
AlanRichard <leavessoft09@gmail.com>
2021-05-31 19:24:18 +0800

    Clean up

--- src/main/java/cc/eumc/GreenFingersListener.java
+++ src/main/java/cc/eumc/GreenFingersListener.java
@@ -18,15 +18,18 @@ public class GreenFingersListener implements Listener {
 
     @EventHandler
     public void onPotionSplash (PotionSplashEvent e) {
-        final Material plant = GreenFingers.getTypeOfFlowerGas(e.getPotion().getItem());   // planet would be the target planet type if the potion is a GreenFingers one
+        // TODO Check if the player can build here
         final Location hitLocation = e.getEntity().getLocation();
+        final Material plant = GreenFingers.getTypeOfFlowerGas(e.getPotion().getItem());   // planet would be the target planet type if the potion is a GreenFingers one
+//        final Block blockBeneath = hitLocation.getWorld().getBlockAt(hitLocation.getBlockX(), hitLocation.getBlockY() - 1, hitLocation.getBlockZ());
+
         if (plant != null && hitLocation != null) {                                           // | is it a GreenFingers item?
             new BukkitRunnable() {
                 @Override
                 public void run() {
                     GreenFingers.sendInfo("Planted " +
                             "" + genPlant(hitLocation, plant) +
-                            (plant.toString()=="AIR"?"MIX":plant.toString()) );
+                            (plant.toString().equals("AIR") ?"MIX":plant.toString()) );
                 }
             }.runTaskLater(instance, 1);
         }
@@ -38,22 +41,22 @@ public class GreenFingersListener implements Listener {
             return 0;
         }
 
-        Integer radius = instance.getConfig().getInt("Settings.Garden.Gas.Radius");
-        Integer flowerCount = 0;
+        int radius = instance.getConfig().getInt("Settings.Garden.Gas.Radius");
+        int flowerCount = 0;
 
-        Integer bx = centerBlock.getBlockX();
-        Integer by = centerBlock.getBlockY();
-        Integer bz = centerBlock.getBlockZ();
+        int bx = centerBlock.getBlockX();
+        int by = centerBlock.getBlockY();
+        int bz = centerBlock.getBlockZ();
 
         List<Material> flowerList = (plant == Material.AIR)? GreenFingers.getFlowerList() : null;
         if (plant == Material.AIR && flowerList == null) {
             return 0;
         }
 
-        if (instance.getConfig().getString("Settings.Garden.Gas.Shape.Type").equalsIgnoreCase( "Star")) {
+        if (instance.getConfig().getString("Settings.Garden.Gas.Shape.Type").equalsIgnoreCase( "star")) {
             StarPolygon star = new StarPolygon(0, 0, radius, (int)(radius*0.618), instance.getConfig().getInt("Settings.Garden.Gas.Shape.VertexCount", 5));
-            for (Integer y = (int)(centerBlock.getY() - (radius/2)); y <= (int)(centerBlock.getY() + (radius/2)); y++) {
-                Integer i = 0; // index of xpoints
+            for (int y = (int)(centerBlock.getY() - (radius/2)); y <= (int)(centerBlock.getY() + (radius/2)); y++) {
+                int i = 0; // index of xpoints
                 for (Integer x : star.xpoints) {
                     Location targetLocation = new Location(centerBlock.getWorld(), bx + x, y, bz + star.ypoints[i]);
                     i++;
@@ -62,9 +65,9 @@ public class GreenFingersListener implements Listener {
             }
         }
         else {
-            for (Integer x = bx - radius; x <= bx + radius; x++) {
-                for (Integer y = by - radius; y <= by + radius; y++) {
-                    for (Integer z = bz - radius; z <= bz + radius; z++) {
+            for (int x = bx - radius; x <= bx + radius; x++) {
+                for (int y = by - radius; y <= by + radius; y++) {
+                    for (int z = bz - radius; z <= bz + radius; z++) {
 
                         double distance = ((bx - x) * (bx - x) + ((bz - z) * (bz - z)) + ((by - y) * (by - y)));
 
